b25b5e2293486379fb071cdb3cc59a64c6bde67e
wahid.anwar <wahid.anwar1686@gmail.com>
2026-01-11 19:01:35 +0600

    Added dockerfile and compose yaml

--- ai-document-intelligence/src/main/java/io/wahid/publication/ai/ServerLauncher.java
+++ ai-document-intelligence/src/main/java/io/wahid/publication/ai/ServerLauncher.java
@@ -5,11 +5,13 @@ import io.wahid.publication.ai.api.QueryServlet;
 import io.wahid.publication.ai.service.impl.StubIngestionService;
 import io.wahid.publication.ai.service.impl.StubQueryService;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
+import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.server.Server;
 
 public class ServerLauncher {
 
-    public static void main(String[] args) throws Exception {
+/*    public static void main(String[] args) throws Exception {
+        System.out.println("Inside serverlauncher");
         ApplicationContext applicationContext = new ApplicationContext();
 
         IngestServlet ingestServlet = new IngestServlet(applicationContext.ingestionService());
@@ -35,5 +37,30 @@ public class ServerLauncher {
         server.setHandler(context);
         server.start();
         server.join();
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Inside serverlauncher");
+
+        ApplicationContext applicationContext = new ApplicationContext();
+
+        IngestServlet ingestServlet = new IngestServlet(applicationContext.ingestionService());
+
+        QueryServlet queryServlet = new QueryServlet(applicationContext.queryService());
+
+        Server server = new Server(8080);
+
+        ServletContextHandler context =
+                new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/api");
+
+        context.addServlet(new ServletHolder(ingestServlet), "/ingest");
+        context.addServlet(new ServletHolder(queryServlet), "/query");
+
+        server.setHandler(context);
+
+        server.start();
+        server.join(); // ✔ correct and required
     }
+
 }
